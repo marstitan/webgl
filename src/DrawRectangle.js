4f1b3c296ee1e241e3cef63db5c3aa31b313e328
dami <154863017@qq.com>
2017-01-06 21:21:01 +0800

    u

--- src/DrawRectangle.js
+++ src/DrawRectangle.js
@@ -13,7 +13,27 @@ var FSHADER_SOURCE =
     'void main(){\n'+
     'gl_FragColor = u_FragColor;\n'+
     '}\n';
+function initVertexBuffers(gl){
+    var vertices = new Float32Array([0.0,0.5,-0.5,-0.5,0.5,-0.5]);
+    var n = 3;
 
+    var vertexBuffer = gl.createBuffer();
+    if(!vertexBuffer){
+        console.log('Failed to create buffer object');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
+    gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0);
+
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+
+}
 function main(){
     var canvas = document.getElementById("example");
     if(!canvas){
@@ -29,35 +49,44 @@ function main(){
         console.log("fail to init shaders");
         return false;
     }
-    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
     var u_FragColor = gl.getUniformLocation(gl.program,'u_FragColor');
-    canvas.onmousedown = function(evt){
-        click(evt,gl,canvas,a_Position,u_FragColor);
-    };
-    var g_points = [];
-    var g_colors = [];
-    function click(evt,gl,canvas,a_Position,u_FragColor){
-        var x = evt.clientX;
-        var y = evt.clientY;
-        var rect = evt.target.getBoundingClientRect();
+    gl.uniform4f(u_FragColor,1.0,0.0,0.0,1.0);
 
-        x = (x - rect.left - canvas.width/2)/(canvas.width/2);
-        y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-        g_points.push({
-            x:x,y:y
-        });
-
-        g_colors.push([Math.random(),Math.random(),Math.random(),1.0]);
-
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        for(var i = 0;i < g_points.length;i++){
-            var point = g_points[i];
-            var color = g_colors[i];
-            gl.vertexAttrib2f(a_Position,point.x,point.y);
-            gl.uniform4f(u_FragColor,color[0],color[1],color[2],color[3]);
-            gl.drawArrays(gl.Points,0,1);
-        }
+    var n = initVertexBuffers(gl);
+    if(n < 0){
+        console.log('Failed to set buffer object');
+        return;
     }
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS,0,n);
+
+    //canvas.onmousedown = function(evt){
+    //    click(evt,gl,canvas,a_Position,u_FragColor);
+    //};
+    //var g_points = [];
+    //var g_colors = [];
+    //function click(evt,gl,canvas,a_Position,u_FragColor){
+    //    var x = evt.clientX;
+    //    var y = evt.clientY;
+    //    var rect = evt.target.getBoundingClientRect();
+    //
+    //    x = (x - rect.left - canvas.width/2)/(canvas.width/2);
+    //    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    //    g_points.push({
+    //        x:x,y:y
+    //    });
+    //
+    //    g_colors.push([Math.random(),Math.random(),Math.random(),1.0]);
+    //
+    //    gl.clear(gl.COLOR_BUFFER_BIT);
+    //    for(var i = 0;i < g_points.length;i++){
+    //        var point = g_points[i];
+    //        var color = g_colors[i];
+    //        gl.vertexAttrib2f(a_Position,point.x,point.y);
+    //        gl.uniform4f(u_FragColor,color[0],color[1],color[2],color[3]);
+    //        gl.drawArrays(gl.Points,0,1);
+    //    }
+    //}
+
 }
