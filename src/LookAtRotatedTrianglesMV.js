9e4609d83878fb55bfeb1ca767d87c131761b4f1
wfl <154863017@qq.com>
2017-02-04 22:00:54 +0800

    u

---
+++ src/LookAtRotatedTrianglesMV.js
@@ -0,0 +1,99 @@
+/**
+ * Created by mac on 2/4/17.
+ */
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
+    'uniform mat4 u_ModelViewMatrix;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main(){\n' +
+    'gl_Position = u_ModelViewMatrix*a_Position;\n' +
+    'v_Color = a_Color;\n' +
+    '}\n';
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main(){\n' +
+    'gl_FragColor = v_Color;\n' +
+    '}\n';
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.4, 0.4, 1.0, 0.4,
+        -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+        0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+
+        0.5, 0.4, -0.2, 1.0, 0.4, 0.4,
+        -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+        0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+        0.0, 0.5, 0.0, 0.4, 0.4, 1.0,
+        -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+        0.5, -0.5, 0.0, 1.0, 0.4, 0.4
+    ]);
+    var n = 9;
+    var FSIZE = vertices.BYTES_PER_ELEMENT;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create buffer object');
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 6 * FSIZE, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    var colorBuffer = gl.createBuffer();
+    if (!colorBuffer) {
+        console.log('Failed to create buffer object');
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 6 * FSIZE, 3 * FSIZE);
+    gl.enableVertexAttribArray(a_Color);
+
+    return n;
+
+}
+function main() {
+    var canvas = document.getElementById("example");
+    if (!canvas) {
+        console.log("fail to get canvas");
+        return false;
+    }
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log("fail to get gl context");
+        return false;
+    }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log("fail to init shaders");
+        return false;
+    }
+
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set buffer object');
+        return;
+    }
+
+    var viewMatrix = new Matrix4();
+    viewMatrix.setLookAt(0.2, 0.25, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+    var modelMatrix = new Matrix4();
+    modelMatrix.setRotate(-10,0,0,1);
+    var modelViewMatrix = viewMatrix.multiply(modelMatrix);
+
+    var u_ModelViewMatrix = gl.getUniformLocation(gl.program, 'u_ModelViewMatrix');
+    gl.uniformMatrix4fv(u_ModelViewMatrix,false,modelViewMatrix.elements);
+
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+
+}
